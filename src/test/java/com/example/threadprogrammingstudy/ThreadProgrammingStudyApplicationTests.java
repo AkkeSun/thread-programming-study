3a901ac1ed83515201d426151b62903f54a7a18c
od <od@sweettracker.co.kr>
2023-07-28 15:34:29 +0900

    first commit

---
+++ src/test/java/com/example/threadprogrammingstudy/ThreadProgrammingStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.threadprogrammingstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadProgrammingStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
